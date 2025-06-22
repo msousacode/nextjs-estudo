bb72c44e4b88ada9e47fdb991fc9eb7b9a84d1d5
MSousa <msousacode@gmail.com>
2025-06-22 20:47:09 -0300

    chore: criacao do loading

--- src/app/posts-server/[id]/loading.tsx
+++ src/app/posts-server/[id]/loading.tsx
@@ -1,3 +1,11 @@
+//Esse loading é para todo o componente
 export default function Loading() {
-  return <div>Carregando...</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <div className="flex flex-col items-center">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid mb-4"></div>
+        <span className="text-gray-700 text-lg font-semibold">Carregando...</span>
+      </div>
+    </div>
+  );
 }
